26a571e2217837669914d31e44b63909e06dca7f
Kavya Medasani <kavyatechie15@gmail.com>
2026-08-10 14:18:40 -0500

    Add health check HTTP endpoint

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	response := map[string]string{
+		"status": "ok",
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
 
 func main() {
-	fmt.Println("QueueFlow is starting...")
+	http.HandleFunc("/health", healthHandler)
+
+	log.Println("QueueFlow server starting on http://localhost:8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
